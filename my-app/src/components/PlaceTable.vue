18331d16bc16a595b38c9c6ba3a11856e9182946
haonguyen3817884 <S3817884@rmit.edu.vn>
2022-05-08 13:12:09 +0700

    change structure and update style for component

---
+++ my-app/src/components/PlaceTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="margin: 17px 0px">
+    place
+    <div>{{ textValue.first }}</div>
+    <div v-bind:style="placeStyle">{{ textValue.last }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlaceTable",
+  props: {
+    textValue: {
+      type: Object,
+      default: function () {
+        return {};
+      },
+    },
+  },
+  computed: {
+    placeStyle: function () {
+      return this.textValue.isPlace ? { color: "red" } : { color: "#111111" };
+    },
+  },
+};
+</script>
